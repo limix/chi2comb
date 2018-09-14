5bc3600180436478cde6d9ece81c71e7ebe3cb65
Danilo Horta <danilo.horta@gmail.com>
2018-09-14 11:22:02 +0100

    make test working, result matching with R

--- test/chi2comb.c
+++ test/chi2comb.c
@@ -1,39 +1,31 @@
 #include "chi2comb.h"
+#include "acutest.h"
 #include <float.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
-int test_chi2comb() {
+#define EPSILON 1e-12
 
-    double coef[] = {0.4, 1.1};
-    double nc[] = {-1.3, 2.9};
-    int dof[] = {1, 3};
+void test_chi2comb(void) {
+
+    double coefs[] = {0.4, 1.1};
+    double ncentrals[] = {1.3, 2.9};
+    int dofs[] = {1, 3};
     int ncoefs = 2;
     double coef1 = 0.41;
     double dof_eval = 2.0;
     int lim = 1000;
-    double acc = 1e-8;
+    double acc = 1e-16;
     double info[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     int fault = 0;
     double res = 0.0;
 
-    qfc(coef, nc, dof, &ncoefs, &coef1, &dof_eval, &lim, &acc, info, &fault, &res);
+    qfc(coefs, ncentrals, dofs, &ncoefs, &coef1, &dof_eval, &lim, &acc, info, &fault,
+        &res);
+    res = 1 - res;
 
-    printf("\n");
-    for (int i = 0; i < 7; ++i) {
-        printf("%f ", info[i]);
-    }
-    printf("\n");
-    printf("%d", fault);
-    printf("\n%f\n", res);
-
-    return 0;
+    TEST_CHECK(fabs(res - 0.9183206009070191) < EPSILON);
 }
 
-int main() {
-    test_chi2comb();
-
-    size_t i;
-
-    return 0;
-}
+TEST_LIST = {{"test_chi2comb", test_chi2comb}, {NULL, NULL}};
